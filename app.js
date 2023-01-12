21cb272477e8fdf972808a32c5fd09dc2a8dc0be
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-12 21:09:36 +0200

    Add some basic express examples

--- app.js
+++ app.js
@@ -9,8 +9,41 @@ const app = express();
   delete - удаление данных (DELETE)
 */
 
-app.get('/users', async (req, res, next) => {
-  res.send('hello from users');
+app.use(express.json());
+
+app.get(
+  '/users',
+  async (req, res, next) => {
+    console.log('I`m a middleware');
+    // next(); // нормальный переход дальше
+    next('test'); // произошла ошибка
+  },
+  async (req, res, next) => {
+    res.send('hello from users');
+  }
+);
+
+app.post('/users', async (req, res, next) => {
+  const { body } = req;
+
+  res.send(body);
 });
+
+app.use(
+  async (err, req, res, next) => {
+    console.log(`Error is ${err}`);
+    if (Math.random() > 0.5) {
+      return res.send('Error!');
+    }
+    next(err);
+  },
+  async (err, req, res, next) => {
+    console.log(`Error is ${err}`);
+    if (Math.random() > 0.5) {
+      return res.send('Error!');
+    }
+    next(err);
+  }
+);
 
 module.exports = app;
