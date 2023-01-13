b7e7445b13d79a5eb26585e743f2a9d0673d109e
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-13 20:59:25 +0200

    Add car model

---
+++ models/car.js
@@ -0,0 +1,78 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Car extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Car.init(
+    {
+      model: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+          isAlphanumeric: true,
+          notEmpty: true,
+        },
+      },
+      manufacturer: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+          isAlpha: true,
+          notEmpty: true,
+        },
+      },
+      modelYear: {
+        type: DataTypes.INTEGER,
+        validate: {
+          isInt: true,
+        },
+      },
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          isNumeric: true,
+          notNull: true,
+          notEmpty: true,
+          isCorrectPrice(newPrice) {
+            if (newPrice < 0) {
+              throw new Error('Price cannot be this low');
+            }
+          },
+        },
+      },
+      isUsed: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      serialNumber: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          notNull: true,
+          isAlphanumeric: true,
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Car',
+      tableName: 'cars',
+      underscored: true,
+    }
+  );
+  return Car;
+};
