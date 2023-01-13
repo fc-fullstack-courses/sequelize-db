f3f56623a26980cc21ea1575b6528dff6a1fcf10
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-13 21:15:05 +0200

    Add basic post route

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
-
+const { Car } = require('./models');
 const app = express();
 
 app.use(express.json());
+
+app.post('/cars', async (req, res, next) => {
+  const { body } = req;
+
+  const newCar = await Car.create(body);
+
+  res.send({ car: newCar });
+});
+
+// const cars = await Car.findAll();
 
 module.exports = app;
