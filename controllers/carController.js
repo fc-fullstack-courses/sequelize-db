465f52c5ec96e864311cf32e78a54884e7199a2c
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-16 20:05:34 +0200

    Add Get all cars route

--- controllers/carController.js
+++ controllers/carController.js
@@ -7,3 +7,18 @@ module.exports.createCar = async (req, res, next) => {
 
   res.status(201).send({ data: newCar });
 };
+
+module.exports.getCars = async (req, res, next) => {
+  const cars = await Car.findAll({
+    // attributes: ['model', 'manufacturer', 'isUsed', 'price']
+    // attributes: {
+    //   exclude: ['updatedAt']
+    // }
+    // where: {
+    //   isUsed: false,
+    //   model: 'Astra'
+    // },
+  });
+
+  res.send({ data: cars });
+};
--- routers/index.js
+++ routers/index.js
@@ -3,5 +3,6 @@ const CarController = require('../controllers/carController');
 const router = express.Router();
 
 router.post('/cars', CarController.createCar);
+router.get('/cars', CarController.getCars);
 
 module.exports = router;
