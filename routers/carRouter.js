a880d5f790813e182d65b3544f76161adfa75e3e
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-17 20:59:47 +0200

    Add review router

--- routers/carRouter.js
+++ routers/carRouter.js
@@ -1,5 +1,7 @@
 const carRouter = require('express').Router();
+const reviewRouter = require('./reviewRouter');
 const CarController = require('../controllers/carController');
+const CarMW = require('../middlewares/carMW');
 
 carRouter.post('/', CarController.createCar);
 carRouter.get('/', CarController.getCars);
@@ -9,5 +11,7 @@ carRouter.put('/:carId', CarController.updateCar1);
 carRouter.put('/v2/:carId', CarController.updateCar1);
 carRouter.delete('/:carId', CarController.deleteCar1);
 carRouter.delete('/v2/:carId', CarController.deleteCar2);
+
+carRouter.use('/:carId/reviews', CarMW.getCar, reviewRouter);
 
 module.exports = carRouter;
---
+++ routers/reviewRouter.js
@@ -0,0 +1,7 @@
+const reviewRouter = require('express').Router();
+const ReviewController = require('../controllers/reviewController');
+
+reviewRouter.post('/', ReviewController.createReview);
+reviewRouter.get('/', ReviewController.getReviews);
+
+module.exports = reviewRouter;
