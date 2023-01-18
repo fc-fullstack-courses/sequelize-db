2e7a9bafe0d61a8935d4b1a667c24ca63f2f4500
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-18 19:14:16 +0200

    Add sellers migrations

---
+++ migrations/20230118170302-create-cars-to-sellers.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('cars_to_sellers', {
+      carId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        field: 'car_id',
+        references: {
+          model: 'cars',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      sellerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        field: 'seller_id',
+        references: {
+          model: 'sellers',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at',
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('cars_to_sellers');
+  },
+};
